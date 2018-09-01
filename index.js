86579e7ed8fac4f0c869103b0ad2cf1543ba3459
Benjamin Howe <bdhowe@gmail.com>
2018-09-01 17:58:37 -0600

    implement audio tree factory and some audio nodes

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ import Machine from "./lib/Machine.js";
 import invention, {setup} from "./lib/rolls/invention.js";
 
 window.machine = new Machine(window);
-machine.setVolume(0.2);
 
 machine.loadSetup(setup);
